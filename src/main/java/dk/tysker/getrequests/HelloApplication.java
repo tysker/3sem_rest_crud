8ade700673e8c88587743c6f52d87625a3b0db96
tysker <tyskerjoerg@gmail.com>
2022-02-15 11:43:47 +0100

    first commit

---
+++ src/main/java/dk/tysker/getrequests/HelloApplication.java
@@ -0,0 +1,9 @@
+package dk.tysker.getrequests;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
